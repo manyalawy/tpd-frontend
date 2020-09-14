e9263c0dc00a8be4bd9cdcc18024cc0c848e24c4
manyalawy <manyalawy@gmail.com>
2020-09-15 00:13:43 +0200

    initial commit

---
+++ src/Components/nav-bar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import style from './nav-bar.css'
+import profile from '../assets/profile.png'
+import Button from '@material-ui/core/Button';
+
+
+function NavBar() {
+  return (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  )
+}
+
+export default NavBar;
