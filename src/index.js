7c2abaa65ea5cb33a4d66f7f61949c1a1487561f
manyalawy <manyalawy@gmail.com>
2020-09-15 00:42:43 +0200

    changed filed structure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import NavBar from './Components/nav-bar.js'
+import NavBar from './Components/NavigationBar/nav-bar.js'
 
 
 ReactDOM.render(
  <NavBar/> ,
   document.getElementById('root')
 );
-
-
