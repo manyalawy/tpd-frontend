7716336257da531aef3bc19413aba8916fd08a48
Antony Ayoub <antonywaseem@gmail.com>
2020-10-01 04:24:05 +0200

    Guards for authorization

---
+++ src/Components/Guards/guard.js
@@ -0,0 +1,10 @@
+/**
+ * The purpose of guard is to dispaly content base on an arbitrary condition
+ *
+ */
+
+const Guard = (props) => {
+  return props.condition ? props.children : null;
+};
+
+export default Guard;
